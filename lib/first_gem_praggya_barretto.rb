5c10029887809db30742126f4e5fa2656f044e95
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-12 11:38:32 -0500

    added unique words method

--- lib/first_gem_praggya_barretto.rb
+++ lib/first_gem_praggya_barretto.rb
@@ -1,5 +1,11 @@
 require "first_gem_praggya_barretto/version"
 
-module FirstGemPraggyaBarretto
-  # Your code goes here...
+class String
+	def word_count
+		self.split.count
+	end
+
+	def unique_words
+		self.split.uniq
+	end
 end
